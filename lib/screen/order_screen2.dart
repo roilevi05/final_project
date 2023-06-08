d9c3d56a31a32dcb1f4d136d9bad56e388869796
roilevi05 <122961241+roilevi05@users.noreply.github.com>
2023-06-08 21:09:36 +0300

    Update order_screen2.dart

--- lib/screen/order_screen2.dart
+++ lib/screen/order_screen2.dart
@@ -10,8 +10,6 @@ import '../providers/auth_provider.dart';
 import '../providers/finalorder_provider.dart';
 import '../providers/product_provider.dart';
 
-//Product product טענת כניסה : הפעולה מקבלת
-//_OrderScreenState טענת יציאה : מזמן את המחלקה של
 class OrderScreen extends StatefulWidget {
   final Product product;
   OrderScreen(@required this.product);
@@ -21,110 +19,200 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  List<finalOrder> list = [];
+  int y = 0;
+  String check = '';
 
-  List<finalOrder> list1 = [];
-  //BuildContext context טענת כניסה : פעולה שמקבלת
-  //טענת יציאה :פעולה שבונה מסך ובו אנו מוסיפים את המוצר להזמנה
+  Future<void> showErrorDialog() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error message'),
+          content: Text('You encountered an error.'),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     List<finalOrder> listOfOrders =
         Provider.of<FinalOrderProvider>(context, listen: false).allOrders;
     String uid = Provider.of<AuthProvdier>(context, listen: false).getuid();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.product.name),
-          backgroundColor: Colors.blue,
-        ),
-        body: Column(
-          children: <Widget>[
-            Image(
-              image: NetworkImage(widget.product.picture),
-              width: double.infinity,
-              height: 400,
-            ),
-            Container(
-              child: Text(widget.product.description),
-              margin: const EdgeInsets.all(15.0),
-              padding: const EdgeInsets.all(1.0),
-              width: 200,
-              height: 100,
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-            ),
-            Center(
-              child: Center(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: 150,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(15.0),
-                      padding: const EdgeInsets.all(1.0),
-                      width: 100,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black)),
-                      child: Center(child: Text('${widget.product.price}\$')),
-                    ),
-                  ],
+      appBar: AppBar(
+        title: Text(widget.product.name),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.network(
+            widget.product.picture,
+            width: double.infinity,
+            height: 400,
+          ),
+          Container(
+            child: Text(widget.product.description),
+            margin: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(1.0),
+            width: 200,
+            height: 100,
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          ),
+          Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: SizedBox()),
+                Container(
+                  margin: const EdgeInsets.all(15.0),
+                  padding: const EdgeInsets.all(1.0),
+                  width: 100,
+                  height: 50,
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Center(child: Text('${widget.product.price}\$')),
                 ),
-              ),
+              ],
             ),
-            ElevatedButton(
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: Colors.blue),
-              child: Text('add order'),
-              onPressed: () {
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(),
+            child: Text('Add Order'),
+            onPressed: () async {
+              if (check == '') {
                 int sum = 0;
                 for (finalOrder i in listOfOrders) {
                   if (i.userid == uid) {
                     sum++;
+                    print('check');
+                    print(i.isSold);
                     if (i.isSold == 'not pay') {
-                      Provider.of<productInOrderProvider>(context,
-                              listen: false)
-                          .addOrder(widget.product, i.orderid);
+                      try {
+                        await Provider.of<productInOrderProvider>(context,
+                                listen: false)
+                            .addOrder(widget.product, i.orderid, context);
+                      } catch (error) {
+                        setState(() {
+                          check = error.toString();
+                        });
+                        await showErrorDialog();
+                        return;
+                      }
+                      break;
                     } else {
-                      Provider.of<FinalOrderProvider>(context, listen: false)
-                          .addOrderData(uid);
-                      Provider.of<FinalOrderProvider>(context, listen: false)
-                          .returnId(uid)
-                          .then((value) {
-                        String orderid = Provider.of<FinalOrderProvider>(
-                                context,
+                      try {
+                        await Provider.of<FinalOrderProvider>(context,
                                 listen: false)
-                            .getorderId();
+                            .addOrderData(uid)
+                            .then((value) async {
+                          try {
+                            await Provider.of<FinalOrderProvider>(context,
+                                    listen: false)
+                                .returnId(uid)
+                                .then((value) async {
+                              String orderid = Provider.of<FinalOrderProvider>(
+                                      context,
+                                      listen: false)
+                                  .getorderId();
 
-                        Provider.of<productInOrderProvider>(context,
-                                listen: false)
-                            .addOrder(widget.product, orderid);
-                      });
+                              try {
+                                await Provider.of<productInOrderProvider>(
+                                        context,
+                                        listen: false)
+                                    .addOrder(widget.product, orderid, context);
+                              } catch (error) {
+                                setState(() {
+                                  check = error.toString();
+                                });
+                                await showErrorDialog();
+                                return;
+                              }
+                            });
+                          } catch (error) {
+                            setState(() {
+                              check = error.toString();
+                            });
+                            await showErrorDialog();
+                            return;
+                          }
+                        });
+                      } catch (error) {
+                        setState(() {
+                          check = error.toString();
+                        });
+                        await showErrorDialog();
+                        return;
+                      }
                     }
                   }
                 }
 
                 if (sum == 0) {
-                  Provider.of<FinalOrderProvider>(context, listen: false)
-                      .addOrderData(uid);
+                  try {
+                    await Provider.of<FinalOrderProvider>(context,
+                            listen: false)
+                        .addOrderData(uid)
+                        .then((value) async {
+                      try {
+                        await Provider.of<FinalOrderProvider>(context,
+                                listen: false)
+                            .returnId(uid)
+                            .then((value) async {
+                          String orderid = Provider.of<FinalOrderProvider>(
+                                  context,
+                                  listen: false)
+                              .getorderId();
 
-                  Provider.of<FinalOrderProvider>(context, listen: false)
-                      .returnId(uid)
-                      .then((value) {
-                    String orderid =
-                        Provider.of<FinalOrderProvider>(context, listen: false)
-                            .getorderId();
-
-                    Provider.of<productInOrderProvider>(context, listen: false)
-                        .addOrder(widget.product, orderid);
-                  });
+                          try {
+                            await Provider.of<productInOrderProvider>(context,
+                                    listen: false)
+                                .addOrder(widget.product, orderid, context);
+                          } catch (error) {
+                            setState(() {
+                              check = error.toString();
+                            });
+                            await showErrorDialog();
+                            return;
+                          }
+                        });
+                      } catch (error) {
+                        setState(() {
+                          check = error.toString();
+                        });
+                        await showErrorDialog();
+                        return;
+                      }
+                    });
+                  } catch (error) {
+                    setState(() {
+                      check = error.toString();
+                    });
+                    await showErrorDialog();
+                    return;
+                  }
                 }
-                Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+              }
+
+              if (check != '') {
+                await showErrorDialog();
+              } else {
+                await Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) {
                   return TabBottomAdmin('client');
                 }));
-              },
-            ),
-          ],
-        ));
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
