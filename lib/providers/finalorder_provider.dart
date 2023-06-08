2ab29d017adb1682af5e8b54997edffd6c43bc4a
roilevi05 <122961241+roilevi05@users.noreply.github.com>
2023-06-08 21:05:10 +0300

    Update finalorder_provider.dart

--- lib/providers/finalorder_provider.dart
+++ lib/providers/finalorder_provider.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/models/final_order.dart';
 import 'package:flutter_complete_guide/models/order.dart';
-import 'package:flutter_complete_guide/screen/erorrMessage.dart';
+import 'package:flutter_complete_guide/models/product.dart';
 
 import '../screen/tab_bottom_admin.dart';
 
@@ -43,19 +43,16 @@ class FinalOrderProvider with ChangeNotifier {
   //String userId טענת כניסה :פעולה שמקבלת
 // order בטבלה של  Firebase פעולה שמטרתה להוסיף נתונים ל
 
-  Future<Widget> addOrderData(String userId) async {
-    try {
-      FirebaseFirestore.instance.collection('order').add({
-        'userid': userId,
-        'issold': 'not pay',
-        'time': DateTime.now()
-      }).then((DocumentReference doc) {
-        _allOrders.add(finalOrder(userId, Timestamp.now(), 'not pay', doc.id));
-        uid = doc.id;
-      });
-      notifyListeners();
-    } catch (erorr) {
-    }
+  Future<String> addOrderData(String userId) async {
+    FirebaseFirestore.instance.collection('order').add({
+      'userid': userId,
+      'issold': 'not pay',
+      'time': DateTime.now(),
+    }).then((DocumentReference doc) {
+      _allOrders.add(finalOrder(userId, Timestamp.now(), 'not pay', doc.id));
+      uid = doc.id;
+    });
+    notifyListeners();
   }
 
   var collection = FirebaseFirestore.instance.collection('order');
@@ -68,22 +65,19 @@ class FinalOrderProvider with ChangeNotifier {
 
 //String uid טענת כניסה : פעולה שמקבלת
 // Firebaseשל ההזמנה מה idטענת יציאה: פעולה שמטרתה לקבל את ה
-  Future<Widget> returnId(String uid) async {
-    try {
-      var getCollection = await collection.get();
-      var getId = getCollection.docs
-          .firstWhere((element) =>
-              element['userid'] == uid && element['issold'] == 'not pay')
-          .id;
-      this.orderId = getId.toString();
-      notifyListeners();
-    } catch (erorr) {
-    }
+  Future<String> returnId(String uid) async {
+    var getCollection = await collection.get();
+    var getId = getCollection.docs
+        .firstWhere((element) =>
+            element['userid'] == uid && element['issold'] == 'not pay')
+        .id;
+    this.orderId = getId.toString();
+    notifyListeners();
   }
 
 //String id, String state טענת כניסה : פעולה שמקבלת
 //Firebase טענת יציאה : פעולה שמטרתה לבדוק אם צב המוצר ולשנות אותו בהתאם לקלט ב
-  Future<Widget> changeState(String id, String state) async {
+  Future<String> changeState(String id, String state) async {
     try {
       FirebaseFirestore.instance
           .collection('order')
@@ -91,7 +85,9 @@ class FinalOrderProvider with ChangeNotifier {
           .update({"issold": state}).then((result) {
         print("new USer true");
       });
+      return '';
     } catch (erorr) {
+      return erorr.toString();
     }
   }
 
