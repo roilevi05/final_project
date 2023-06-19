43616e323929c65584adfb4cd475489741ab5124
roilevi05 <122961241+roilevi05@users.noreply.github.com>
2023-06-19 19:30:10 +0300

    Update auth.dart

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Auth with ChangeNotifier {
-  String uid;
-  String email;
-  String userName;
-  String phone;
-  String kind;
-  String url;
+  String uid=  '';
+  String email = '';
+  String userName = '';
+  String phone = '';
+  String kind = '';
+  String url = '';
   //String email, String userName, String phone, String uid, String kind, String url טענת כניסה : פעולה שמקבלת
   //טענת יציאה : הפעולה יוצרת משתמש  חדש
   Auth(String email, String userName, String phone, String uid, String kind,
