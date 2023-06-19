dc70405bd862a5ab9b612cb02daaeb5dcb765704
roilevi05 <122961241+roilevi05@users.noreply.github.com>
2023-06-19 19:31:29 +0300

    Update product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,17 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/models/order.dart';
 
 class Product with ChangeNotifier {
-  String picture;
-  String id;
-  String name;
-  double price;
-  Timestamp time;
-  String cat;
-  String description;
-  bool delete;
-  bool isUpdated;
+  String picture = '';
+  String id = '';
+  String name = '';
+  double price=0;
+  Timestamp time=Timestamp.now();
+  String cat = '';
+  String description = '';
+  bool delete = false;
+  bool isUpdate =false;
 
   //String name, double price, String cat, Timestamp time, String id,String picture, String description, bool delete טענת כניסה : פעולה שמקבלת
   //טענת יציאה : הפעולה יוצרת מוצר חדש
@@ -22,7 +21,7 @@ class Product with ChangeNotifier {
     this.time = time;
     this.cat = cat;
     this.delete = delete;
-    this.isUpdated = isUpdated;
+    this.isUpdate = isUpdated;
 
     this.id = id;
     this.picture = picture;
@@ -36,7 +35,7 @@ class Product with ChangeNotifier {
   }
 
   void updateProduct() {
-    this.isUpdated = !isUpdated;
+    this.isUpdate = !isUpdate;
     notifyListeners();
   }
 }
