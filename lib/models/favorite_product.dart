6281d571512d9fdebd19c8ab89ccf0b60353173f
roilevi05 <122961241+roilevi05@users.noreply.github.com>
2023-06-19 19:30:54 +0300

    Update favorite_product.dart

--- lib/models/favorite_product.dart
+++ lib/models/favorite_product.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/material.dart';
 
 class FavoriteProduct {
-  String id;
-  String userId;
-  String productId;
+  String id = '';
+  String userId = '';
+  String productId = '';
   //String userId, String productId, String id טענת כניסה : פעולה שמקבלת
   //טענת יציאה : הפעולה יוצרת מוצר אהוב חדש
   FavoriteProduct(String userId, String productId, String id) {
